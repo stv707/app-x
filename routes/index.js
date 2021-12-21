9d2cd8492e073c2bdba9c7a06b82af0269981352
stv707 <stv707@gmail.com>
2021-12-21 16:20:42 +0800

    v2

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,7 @@ var tmem = os.totalmem();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-  res.render('index', {page:'Azure | Redhat Default WebApp - steven.com.my', menuId:'home', sakura: myhost , mazda: cpuc , honda: fmem, bmw: tmem  });
-=======
-  res.render('index', {page:'DO180 Demo WebApp - v1rc', menuId:'home', sakura: myhost , mazda: cpuc , honda: fmem, bmw: tmem  });
->>>>>>> 014abeead282698d9b6cd27c8866b3fcf7692e56
+  res.render('index', {page:'Seagate OCP Demo WebApp - v1rc', menuId:'home', sakura: myhost , mazda: cpuc , honda: fmem, bmw: tmem  });
 });
 
 router.get('/express', function(req, res, next) {
